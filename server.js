1a983d27d4c46d366bf202b4d44f689fd5512970
nicolasjoya-dev <nicolasjoya@orlandofalsbordaied.edu.co>
2026-05-05 17:58:27 -0500

    esperofuncione

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 process.env.TZ = 'America/Bogota';
 const express = require('express');
 const app = express();
+const iaFactura = require('./routes/iaFactura');
+app.use('/api/ia', iaFactura);
 app.use(express.static('public'));
 app.listen(process.env.PORT || 3000);
