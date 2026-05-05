b178d8cd032a7a4a111e4817f9d213342fc11645
nicolasjoya-dev <nicolasjoya@orlandofalsbordaied.edu.co>
2026-05-05 18:30:27 -0500

    er

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
-
 process.env.TZ = 'America/Bogota';
-const express   = require('express');
-const app       = express();
-const iaFactura = require('./routes/iaFactura');
-
-app.use(express.json());
-app.use('/api/ia', iaFactura);
+const express = require('express');
+const app = express();
 app.use(express.static('public'));
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Servidor listo en puerto', process.env.PORT || 3000);
-});
+app.listen(process.env.PORT || 3000);
